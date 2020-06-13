1733183f35a6f7ba45f8d749f19a3fdcac822d1a
qushot <moto@yoshidumi.co.jp>
2020-06-13 11:03:03 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-context-study
+以下の記事のサンプルコードを実装してみる
+[golangでcontextパッケージを使う](https://www.write-ahead-log.net/entry/2017/04/07/214420)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+var key = "key"
+
+func infiniteLoop(ctx context.Context) {
+	innerCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	for {
+		fmt.Printf("Help! [%s]\n", ctx.Value(key).(string))
+		select {
+		case <-innerCtx.Done():
+			fmt.Println("Exit from hell.")
+			return
+		}
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, key, "value")
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+
+	go infiniteLoop(ctx)
+
+	//ctx.Doneが飛んでくるまでmainプロセスの終了をブロックしている
+	select {
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
+
+// WithDeadlineでのキャンセルは「2秒後をデッドラインにする」となっているので、絶対指定感は薄いが、
+// time.Date(2020, time.June, 20, 10, 0, 0, 0, time.Local) のようなことができるという話
